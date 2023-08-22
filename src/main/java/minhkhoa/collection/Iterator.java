f913db03d1d8484fd536b7233b96f1144cc30944
khoa <truongminhkhoa1999vn@gmail.com>
2023-08-22 16:58:07 +0700

    Chinh package gradle

--- src/main/java/minhkhoa/collection/Iterator.java
+++
@@ -1,6 +0,0 @@
-package com.minhkhoa.collection;
-
-public interface Iterator<T> {
-	boolean hasNext();
-	T next();
-}
--- src/main/java/minhkhoa/data_structure/OrderTraversalType.java
+++
@@ -1,7 +0,0 @@
-package com.minhkhoa.data_structure;
-
-public enum OrderTraversalType {
-    PRE_ORDER,
-    IN_ORDER,
-    POST_ORDER
-}
